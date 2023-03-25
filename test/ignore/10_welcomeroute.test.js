7b1d0aabe4cc47e87409e05a9a2846373803c8b9
Søren Spangsberg Jørgensen (Business Academy SouthWest) <smsj@easv.dk>
2023-03-25 23:41:06 +0100

    Test updates and initial workflow

--- test/ignore/10_welcomeroute.test.js
+++ test/ignore/10_welcomeroute.test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
 const chaiHttp = require('chai-http');
-const server = require('../server');
+const server = require('../../server');
 
 chai.use(chaiHttp);
 
@@ -20,7 +20,7 @@ describe('/First Test Collection', () => {
             expect(res.body).to.be.a('object');
             
             const actualVal = res.body.message;
-            expect(actualVal).to.be.equal('Welcome to the MEN RESTAPI');
+            expect(actualVal).to.be.equal('Welcome to the MEN REST-API');
 
             console.log(res.body.message);
             
