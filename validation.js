12a2fa422b8a8e5a28938413444c3e2ffcbed769
Søren Spangsberg Jørgensen (Business Academy SouthWest) <smsj@easv.dk>
2023-04-28 06:54:25 +0000

    Refresh token

--- validation.js
+++ validation.js
@@ -4,24 +4,24 @@ const jwt = require("jsonwebtoken");
 
 // validating registration
 const registerValidation = (data) => {
-    const schema = Joi.object( 
+    const schema = Joi.object(
         {
             name: Joi.string().min(6).max(255).required(),
             email: Joi.string().min(6).max(255).required(),
-            password: Joi.string().min(6).max(255).required()    
+            password: Joi.string().min(6).max(255).required()
         });
-        return schema.validate(data);
+    return schema.validate(data);
 }
 
 // validating login
- const loginValidation = (data) => {
-     const schema = Joi.object(
-         {
+const loginValidation = (data) => {
+    const schema = Joi.object(
+        {
             email: Joi.string().min(6).max(255).required(),
-            password: Joi.string().min(6).max(255).required()                 
-         });
-         return schema.validate(data);
- }
+            password: Joi.string().min(6).max(255).required()
+        });
+    return schema.validate(data);
+}
 
 // middleware to verify token
 const verifyToken = (req, res, next) => {
@@ -34,10 +34,21 @@ const verifyToken = (req, res, next) => {
     try {
         const verified = jwt.verify(token, process.env.TOKEN_SECRET);
         req.user = verified;
-        next();        
+        next();
     } catch (error) {
-        res.status(400).json({ error: "Token is not valid"});
+        res.status(400).json({ error: "Token is not valid" });
     }
 }
 
-module.exports = { registerValidation, loginValidation, verifyToken };
+
+const verifyRefresh = (email, token) => {
+    try {
+        const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+        return decoded.email === email;
+    } catch (error) {
+        // console.error(error);
+        return false;
+    }
+}
+
+module.exports = { registerValidation, loginValidation, verifyToken, verifyRefresh };
